c0d116dd94d65f73ce8c6c7a787c1d7f571605c6
Aks-l <wiersdalenaksel@gmail.com>
2024-12-14 15:36:03 +0100

    day 14

---
+++ 14/14.py
@@ -0,0 +1,40 @@
+import numpy as np  
+import re
+from functools import reduce
+
+data = []
+maxX, maxY = 101, 103
+pattern = r"-?\d+"
+
+with open('14/input14.txt') as file:
+    while True:
+        try:
+            line = file.readline().strip()
+            x, y, vx, vy = map(int, re.findall(pattern, line))
+            data.append((x, y, vx, vy))
+        except:
+            break
+
+def findend(x, y, vx, vy, seconds):
+    # nympy array swaps (x,y) with (y,x) in visualization
+    endX = (y + vy*seconds) % (maxY)
+    endY = (x + vx*seconds) % (maxX)
+    return (endX, endY)
+
+for i in range(10000): #part 1 uses only i=100
+    endpos = []
+    grid = np.zeros((maxY, maxX), dtype=int)
+    for j in range(len(data)):
+        endpos.append(findend(data[j][0], data[j][1], data[j][2], data[j][3], i))
+    for x,y in endpos:
+        grid[x, y] += 1
+        
+    #cheese: assume the tree uses all robots, therefore none should overlap
+    if not np.any((grid > 1)):
+        print(i)
+
+        ''' part1
+        quadrants = [grid[:maxY//2,:maxX//2],grid[maxY//2+1:, :maxX//2], grid[maxY//2+1:, maxX//2+1:], grid[:maxY//2, maxX//2+1:]]
+        quadSums = [(np.sum(i)) for i in quadrants]
+    print(reduce(lambda x, y: x*y, quadSums))
+        '''
